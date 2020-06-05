8bf49c2469b1bbbb825456c5fc59ec5acc7c7947
Rohit Ambre <rohit.ambre@unotechsoft.com>
2020-06-05 22:06:11 +0530

    added winston for logging

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
-const logger = require('morgan');
+const morgan = require('morgan');
+const winston = require('winston');
 
 const app = express();
 // Modules
@@ -15,8 +16,63 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(helmet());
 
+/**
+ * Winston logger formatter
+ * @returns formatted log string
+ */
+const myFormat = winston.format.printf(({ level, message, timestamp }) => {
+    const date = new Date(timestamp);
+    timestamp = date.toGMTString();
+    return `[${timestamp}] - ${level}: ${message}`;
+});
+
+// Winston logger configuraiton
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        myFormat
+    ),
+    defaultMeta: { service: 'user-service' },
+    exitOnError: false,
+    transports: [
+        // - Write all logs with level `error` and below to `error.log`
+        // - Write all logs with level `info` and below to `combined.log`
+        new winston.transports.File({
+            filename: 'logs/error.log',
+            level: 'error',
+            maxsize: 1024 * 1024 * 10, // 10MB
+        }),
+        new winston.transports.File({
+            filename: 'logs/combined.log',
+            maxsize: 1024 * 1024 * 10, // 10MB
+        }),
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: true,
+            json: false,
+            colorize: true,
+        }),
+    ],
+});
+logger.stream = {
+    write: (message, encoding) => {
+        logger.verbose(message);
+    },
+};
+
 if (process.env.NODE_ENV !== 'production') {
-    app.use(logger('dev'));
+    // app.use(morgan('dev'));
+    app.use(
+        morgan('dev', {
+            stream: logger.stream,
+            // only log error responses
+            // skip: (req, res) => {
+            //     return res.statusCode < 400;
+            // },
+        })
+    );
 }
 
 // Mongo Database connection
@@ -29,11 +85,11 @@ mongoose.connect(process.env.MONGO_URI, {
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-    console.log('We are connected to the database');
+    logger.error('We are connected to the database');
 
     app.use('/api', routes);
 });
 
 app.listen(process.env.NODE_PORT, () => {
-    console.log(`server started on ${process.env.NODE_PORT}`);
+    logger.info(`server started on port ${process.env.NODE_PORT}`);
 });
