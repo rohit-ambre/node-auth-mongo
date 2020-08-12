707e69f7959dcbfc25697537cf4127a00358cb9a
Rohit Ambre <rohit003.ambre@gmail.com>
2020-08-12 16:51:20 +0530

    changed error message format

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', (err) => logger.error(`connection error: ${err.stack}`));
 db.once('open', () => {
     logger.info('We are connected to the database');
 
